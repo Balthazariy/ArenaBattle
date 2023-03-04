b2041c2678f6e6658d7de57d1f4c82415c6bef33
Balthazariy <chebureck77@gmail.com>
2023-03-04 17:48:33 +0200

    add enemy spawning

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,4 +1,6 @@
+using Balthazariy.ArenaBattle.Models;
 using Balthazariy.ArenaBattle.Objects.Base;
+using Balthazariy.ArenaBattle.Objects.Enemies;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +9,13 @@ namespace Balthazariy.ArenaBattle
     public class Spawner : MonoBehaviour
     {
         [Header("General")]
-        [SerializeField] private List<Transform> _spawnPoints;
+        [SerializeField] private Transform _spawnPointParent;
         [SerializeField] private List<EnemyBase> _spawnedEnemies;
         [Space(5)]
         [SerializeField] private Transform _enemyParent;
         [SerializeField] private Players.Player _player;
+
+        private List<Transform> _spawnPoints;
 
         private float _spawnTime = 5.0f;
         private float _currentSpawnTIme;
@@ -24,6 +28,9 @@ namespace Balthazariy.ArenaBattle
 
             _currentSpawnTIme = _spawnTime;
             _isSpawning = false;
+
+            for (int i = 0; i < _spawnPointParent.childCount; i++)
+                _spawnPoints.Add(_spawnPointParent.GetChild(i));
 
             Main.Instance.StartGameplayEvent += StartGameplayEventHandler;
         }
@@ -54,20 +61,22 @@ namespace Balthazariy.ArenaBattle
                 _spawnedEnemies[i].FixedUpdate();
         }
 
-        private Vector2 GetSpawnPointPosition() => _spawnPoints[UnityEngine.Random.Range(0, _spawnPoints.Count)].position;
+        private Vector3 GetSpawnPointPosition() => _spawnPoints[UnityEngine.Random.Range(0, _spawnPoints.Count)].localPosition;
 
         private void SpawnEnemy()
         {
+            EnemiesData data = Resources.Load<EnemiesData>("Models/EnemiesData");
             float chanceToBlueEnemy = UnityEngine.Random.Range(0.0f, 100.0f);
 
             EnemyBase enemy = null;
 
-            //if (chanceToBlueEnemy <= 75.0f)
-            //    //enemy = new RedEnemy();
-            //else if (chanceToBlueEnemy > 75.0f)
-            //            Debug.Log("Blue enemy spawned");
-            //enemy = null;
-
+            if (chanceToBlueEnemy <= 75.0f)
+                enemy = new RedEnemy(_enemyParent, GetSpawnPointPosition(), _player, 10f, 1f, 1f, data.GetEnemyByType(EnemyType.Red));
+            else if (chanceToBlueEnemy > 75.0f)
+            {
+                Debug.Log("Blue enemy spawned");
+                enemy = new RedEnemy(_enemyParent, GetSpawnPointPosition(), _player, 10f, 1f, 1f, data.GetEnemyByType(EnemyType.Red));
+            }
             _spawnedEnemies.Add(enemy);
         }
 
